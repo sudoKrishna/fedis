cce12fd7d2abb9b45353d00d7f6977dc23fa27d3
sudoKrishna <krishnachaudhary2007gt@gmail.com>
2026-04-20 12:40:43 +0530

    update all the json

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+export default {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: {
+          moduleResolution: "bundler",
+          ignoreDeprecations: "6.0",
+          module: "ESNext",
+          target: "ES2020",
+        },
+      },
+    ],
+  },
+};
